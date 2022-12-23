a1dc75e421acfc3aab33017627ea362c80f3a8cf
maksim-maliauka <97878382+maksim-maliauka@users.noreply.github.com>
2022-12-23 14:20:54 +0400

    Revert "Implement table with selection"

--- src/helpers/filterGrid.js
+++
@@ -1,8 +0,0 @@
-const filterGrid = (grid, cells) => 
-    grid.map(row => 
-        row.filter(c => 
-            !cells.some(({rowIndex, colIndex}) => c.rowIndex === rowIndex && c.colIndex === colIndex)
-    )
-);
-
-export default filterGrid;
